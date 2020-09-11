7d1c36a089b6e32dad8ab69a4e4324189d99cd32
rezmarefat <61388835+rezmarefat@users.noreply.github.com>
2020-09-11 14:23:22 -0400

    Add files via upload

---
+++ PRACTICE WITH KOOROSH 1/src/ONE/ONE1.java
@@ -0,0 +1,33 @@
+package ONE;
+
+public class ONE1 {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+//int a=20;
+//int b=30;
+//int c=40;
+//int d=a+1;
+//System.out.println(d);
+//int d=++a;
+//a++;
+//System.out.println("value of a is "+a);
+//int e=(a+b+c)/3;
+//System.out.println(e);
+//for(int a=1; a<=100; a++) {
+//System.out.println(+a);
+//for(int a=1;a<=10;a++) {
+	//System.out.println("for loop value="+a);
+
+//for (int b=10;b>=1;b--) {
+	//System.out.println(b);
+}
+
+
+		
+		
+		
+	}
+
+}
---
+++ PRACTICE WITH KOOROSH 1/src/ONE/array.java
@@ -0,0 +1,34 @@
+package ONE;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]=new int[50];
+		a[1]=15;
+		a[3]=80;
+		a[20]=65;
+	System.out.println(a[3]);
+System.out.println(a[1]);
+System.out.println(a[20]);
+System.out.println(a.length);
+int b[][]=new int [3][4];
+b[1][1]=16;
+b[1][2]=18;
+b[1][3]=20;
+System.out.println(b[1][2]);
+System.out.println(b[1][1]);
+System.out.println(b[1][3]);
+System.out.println(b.length);
+
+
+
+
+
+
+
+
+	}
+	
+
+}
